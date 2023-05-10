9a0b0cbb66bf6d9816d0c6caf19ee277748fc731
lobo <matheus.v.lobo@gmail.com>
2023-05-10 18:59:39 -0300

    BI1763-UPDATE
    subindo projeto inicio

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace CadastroSQL
+{
+    partial class FormulariosCadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BoxNome = new System.Windows.Forms.TextBox();
+            this.BoxTel = new System.Windows.Forms.TextBox();
+            this.BoxEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEnviar.Location = new System.Drawing.Point(244, 328);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(334, 68);
+            this.btEnviar.TabIndex = 0;
+            this.btEnviar.Text = "ENVIAR/CADASTRAR";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            this.btEnviar.Click += new System.EventHandler(this.btEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Formulario de Cadastro c/ SQL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOME :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(74, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "E-mail :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "TELEFONE :";
+            // 
+            // BoxNome
+            // 
+            this.BoxNome.Location = new System.Drawing.Point(244, 125);
+            this.BoxNome.Name = "BoxNome";
+            this.BoxNome.Size = new System.Drawing.Size(345, 20);
+            this.BoxNome.TabIndex = 5;
+            // 
+            // BoxTel
+            // 
+            this.BoxTel.Location = new System.Drawing.Point(244, 239);
+            this.BoxTel.Name = "BoxTel";
+            this.BoxTel.Size = new System.Drawing.Size(345, 20);
+            this.BoxTel.TabIndex = 6;
+            // 
+            // BoxEmail
+            // 
+            this.BoxEmail.Location = new System.Drawing.Point(244, 181);
+            this.BoxEmail.Name = "BoxEmail";
+            this.BoxEmail.Size = new System.Drawing.Size(345, 20);
+            this.BoxEmail.TabIndex = 7;
+            // 
+            // FormulariosCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BoxEmail);
+            this.Controls.Add(this.BoxTel);
+            this.Controls.Add(this.BoxNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btEnviar);
+            this.Name = "FormulariosCadastro";
+            this.Text = "Formulario de Cadastro C# com SQL ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox BoxNome;
+        private System.Windows.Forms.TextBox BoxTel;
+        private System.Windows.Forms.TextBox BoxEmail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CadastroSQL
+{
+    public partial class FormulariosCadastro : Form
+    {
+        public FormulariosCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void btEnviar_Click(object sender, EventArgs e)
+        {
+            //criamos a string de conexao
+            // no Betaone é o nome do seu pc ou servidor,   no catalog é o nome do database
+    
+              SqlConnection conn = new SqlConnection("Data Source=KOTANA\\SQLEXPRESS;Initial Catalog=ShelderPoke;Integrated Security=True");
+            //cria a string de inserção do sql
+            string sql = "INSERT INTO dbo.cadastro(id,nome,email,telefone) VALUES (@id,@nome,@email,@telefone)";
+            Random numeroID = new Random();
+            numeroID.Next();
+
+            try
+            {
+                //cria um objeto do tipo comando passando como parametro a string sql e conn
+                SqlCommand c = new SqlCommand(sql, conn);
+
+                //insere os dados da textbox no comando sql
+                c.Parameters.Add(new SqlParameter("@id", numeroID.Next()));
+                c.Parameters.Add(new SqlParameter("@nome", this.BoxNome.Text));
+                c.Parameters.Add(new SqlParameter("@email", this.BoxEmail.Text));
+                c.Parameters.Add(new SqlParameter("@telefone", this.BoxTel.Text));
+                //abrimos a conexao com o banco de dados
+                conn.Open();
+                //executa o comando sql no banco de dados
+                c.ExecuteNonQuery();
+                //fechamos a conexao
+                conn.Close();
+                MessageBox.Show("Enviados com sucesso!");
+            }catch(SqlException ex)
+            {
+                MessageBox.Show("Ocorreu o erro: "+ ex);
+            }
+
+           
+
+        }
+    }
+}
